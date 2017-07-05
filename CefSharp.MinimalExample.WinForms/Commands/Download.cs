c4cc02742b42f26d53329355b06196cf0920d59f
ghstahl@gmail.com <ghstahl@gmail.com>
2017-07-05 08:47:48 -0700

    update

---
+++ CefSharp.MinimalExample.WinForms/Commands/Download.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using CEF.Custom;
+using Synoptic;
+
+namespace CefSharp.MinimalExample.WinForms.Commands
+{
+    [Command]
+    internal class Download
+    {
+        private IDownloadRepository _downloadRepository;
+
+        private IDownloadRepository DownloadRepository
+        {
+            get { return _downloadRepository ?? (_downloadRepository = new DownloadRepository()); }
+        }
+
+        [CommandAction]
+        public List<DownloadRecord> Records()
+        {
+            var result =  DownloadRepository.Records;
+            return result;
+        }
+    }
+}
