5cacb273b9880925088cce7918e754416e1d455b
ghstahl@gmail.com <ghstahl@gmail.com>
2017-07-12 07:51:17 -0700

    Added program repository for seeing if an app is installed

---
+++ Programs.Repository/IProgramsRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programs.Repository
+{
+    public interface IProgramsRepository
+    {
+        void Load();
+        InstalledApp[] Page(int offset, int count);
+        int Count { get; }
+        bool IsInstalled(string displayName);
+    }
+}
